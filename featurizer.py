022e61e63b1942c0659cdcc77287806f202daff7
Caleb Dame <54949405+calebdame@users.noreply.github.com>
2023-09-13 21:04:33 -0600

    Update featurizer.py

--- featurizer.py
+++ featurizer.py
@@ -2,6 +2,7 @@ import numpy as np
 import nltk
 nltk.download('stopwords')
 nltk.download('vader_lexicon')
+nltk.download('punkt')
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
