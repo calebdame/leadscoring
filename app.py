99bf1578355255f95437c38180eff5a5afc8755f
Caleb Dame <54949405+calebdame@users.noreply.github.com>
2023-09-18 23:25:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,8 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 st.title(page_title)
 st.markdown("Tell us about yourself, and we will send you a calendar invite to chat!")
 
-qs = [
-    "First Name", "Last Name",
-    "Phone Number", "Email Address",
+qs = [ 
+    "Full Name", "Phone Number", "Email Address",
     "Home Country", "What is your Occupation?",
     "What’s happening in your life right now that has you potentially considering becoming a life coach? And ultimately what’s your goal?",
     "What best describes your financial situation?",
@@ -51,39 +50,48 @@ def get_remote_ip() -> str:
         return None
     return session_info.request.remote_ip
 
+
+
 with st.form("Answers"):
-    first_name = st.text_input(qs[0])
-    last_name = st.text_input(qs[1])
-    phone_number = st.text_input(qs[2])
-    email = st.text_input(qs[3])
+            
+    name = st.text_input(qs[0])
+    phone_number = st.text_input(qs[1])
+    email = st.text_input(qs[2])
 
     # A dropdown list of countries. You might want to use a more comprehensive list.
-    home_country = st.selectbox(qs[4], COUNTRIES, index=COUNTRIES.index('United States'))
+    home_country = st.selectbox(qs[3], COUNTRIES, index=COUNTRIES.index('United States'))
 
-    occupation = st.text_input(qs[5])
+    occupation = st.text_input(qs[4])
 
     # A larger text area for long responses
-    long_question_response = st.text_area(qs[6], height=225)
+    long_question_response = st.text_area(qs[5], height=225)
 
-    has_money = st.selectbox(qs[7], money_qs, index=2)
+    has_money = st.selectbox(qs[6], money_qs, index=2)
 
     # A true/false checkbox
-    read_brochure = st.checkbox(qs[8])
+    read_brochure = st.checkbox(qs[7])
 
     # A button to submit the form
     if st.form_submit_button("Sign Up"):
         # Display a message upon submission (you might want to do something more useful with the data)
-        
-        features = [
-            f"{first_name} {last_name}", phone_number, home_country, email, occupation,
-            long_question_response, "cash" in has_money, read_brochure, datetime.datetime.now()
-        ]
-        f = Featurizer(
-            *features
-        )
-        feats = f.generate_feature_dict()
-        score, int_score = model.predict([feats])
-        st.success(f"Thank you for submitting the survey!\n\nYou are more likely to convert than {int_score}% of other leads!\n\nFind your calendar invite below:\n\n[Click Here]({url1 if int_score > 40 else url2})", icon="✅")
+        vals = {
+            "Name": len(name), "Phone": len(phone_number), "Email": len(email),
+            "Occupation": len(occupation), "Long Q": len(long_question_response),
+            "Money": len(has_money)
+        }  
+        if not any(i > 0 for i in vals.values()):    
+            features = [
+                name, phone_number, home_country, email, occupation,
+                long_question_response, "cash" in has_money, read_brochure, datetime.datetime.now()
+            ]
+            f = Featurizer(
+                *features
+            )
+            feats = f.generate_feature_dict()
+            score, int_score = model.predict([feats])
+            st.success(f"Thank you for submitting the survey!\n\nYou are more likely to convert than {int_score}% of other leads!\n\nFind your calendar invite below:\n\n[Click Here]({url1 if int_score > 40 else url2})", icon="✅")
+        # else:
+            
 
 # st.success(st.experimental_get_query_params())
 # from streamlit_gsheets import GSheetsConnection
