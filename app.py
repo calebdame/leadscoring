8023a553fb8a0b596a72f1f97bf87791cbfbce60
Caleb Dame <54949405+calebdame@users.noreply.github.com>
2023-09-22 11:05:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,17 +15,11 @@ page_favicon = "jsicon.png"
 st.set_page_config(page_title=page_name, page_icon = page_favicon)#, layout='wide')
 st.markdown("""
         <style>
-               .css-18e3th9 {
-                    padding-top: 0rem;
-                    padding-bottom: 10rem;
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
                     padding-left: 5rem;
                     padding-right: 5rem;
-                }
-               .css-1d391kg {
-                    padding-top: 3.5rem;
-                    padding-right: 1rem;
-                    padding-bottom: 3.5rem;
-                    padding-left: 1rem;
                 }
         </style>
         """, unsafe_allow_html=True)
