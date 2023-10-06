55e285bbc66182cb1031970125c87ebfcde87c28
Caleb Dame <54949405+calebdame@users.noreply.github.com>
2023-10-05 23:42:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import datetime
 from featurizer import *
-import streamlit.components.v1 as components
+import re
 
 def nav_to(url):
     nav_script = """
@@ -9,7 +9,6 @@ def nav_to(url):
     """ % (url)
     st.write(nav_script, unsafe_allow_html=True)
 
-# Configurations and Styles
 def set_streamlit_config():
     hide_st_style = """
         <style>
@@ -18,12 +17,10 @@ def set_streamlit_config():
         header {visibility: hidden;}
         </style>
         """
-    
     st.set_page_config(page_title="Jay Shetty Sign Up", page_icon="jsicon.png")
     st.markdown(hide_st_style, unsafe_allow_html=True)
     set_max_width(950)
     load_external_styles()
-
 
 def set_max_width(max_width):
     st.markdown(
@@ -39,11 +36,9 @@ def set_max_width(max_width):
     </style>
     """, unsafe_allow_html=True)
 
-
 def load_external_styles():
     with open("style.css") as css:
         st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
-
 
 def display_header():
     hide_img_fs = '''
@@ -61,11 +56,9 @@ def display_header():
         st.image("js_profile.png")
         st.markdown(hide_img_fs, unsafe_allow_html=True)
 
-
 def display_main_content():
     st.markdown("<div align=\"center\">This call will be your gateway to find out more about this program and answer any questions you have! We're excited for you to take this step and look forward to speaking with you.</div><br>", unsafe_allow_html=True)
     st.markdown("##### Provide your information below:")
-
     with st.form("Answers"):
         name, country_code, phone_number = get_user_details()
         email, occupation = get_secondary_details()
@@ -73,7 +66,6 @@ def display_main_content():
         check = st.checkbox("I understand my Enrollment Advisor will call me on the number I provided and at the appointment time I will schedule. I will be ready for the call.*")
         
         process_form(name, country_code, phone_number, email, occupation, long_question_response, has_money, read_brochure, check)
-
 
 def get_user_details():
     cols = st.columns([4, 2, 4])
@@ -85,9 +77,7 @@ def get_user_details():
         country_code = country.split("+")[-1]
     with cols[2]:
         phone_number = st.text_input("Phone Number*")
-    
     return name, country_code, phone_number
-
 
 def get_secondary_details():
     cols2 = st.columns([4, 6])
@@ -95,9 +85,7 @@ def get_secondary_details():
         email = st.text_input("Email Address*")
     with cols2[1]:
         occupation = st.text_input("What is your Occupation?*")
-    
     return email, occupation
-
 
 def get_additional_details():
     long_question_response = st.text_area("What’s happening in your life right now that has you potentially considering becoming a life coach? And ultimately what’s your goal?*", height=160)
@@ -105,12 +93,11 @@ def get_additional_details():
     has_money = st.selectbox("What best describes your financial situation?*", money_qs, index=2)
     b_qs = ["Yes", "No", ""]
     read_brochure = st.selectbox("Please confirm you have read our Program Brochure before booking the Enrollment Interview*", b_qs, index=2)
-
     return long_question_response, has_money, read_brochure
-
 
 def process_form(name, country_code, phone_number, email, occupation, long_question_response, has_money, read_brochure, check):
     if st.form_submit_button("Submit"):
+        phone = re.sub("[^0-9]", "", phone)
         vals = {
             "Full Name*": len(name),
             "Phone Number*": len(phone_number),
@@ -120,12 +107,10 @@ def process_form(name, country_code, phone_number, email, occupation, long_quest
             "What best describes your financial situation?*": len(has_money),
             "Please confirm you have read our Program Brochure before booking the Enrollment Interview*": len(read_brochure)
         }
-
         validate_form(vals, check, name, country_code, phone_number, email, occupation, long_question_response, has_money, read_brochure)
 
-
 def validate_form(vals, check, name, country_code, phone_number, email, occupation, long_question_response, has_money, read_brochure):
-    if all(i != 0 for i in vals.values()) and check and (vals["Phone Number*"] > 7) and (vals["Phone Number*"] < 15):
+    if all(i != 0 for i in vals.values()) and check and (vals["Phone Number*"] >= 7) and (vals["Phone Number*"] <= 15):
         features = [
             name, country_code + " (" + phone_number, None, email, occupation,
             long_question_response, int("cash" in has_money),
@@ -137,12 +122,11 @@ def validate_form(vals, check, name, country_code, phone_number, email, occupati
         thresh = 49
         url1 = "https://pages.jayshettycoaching.com/test-jscs-qualified-booking/"
         url2 = "https://pages.jayshettycoaching.com/test-jscs-unqualified-lead/"
-        st.success(f"Thank you for submitting the survey!\n\nYou are more likely to convert than {int_score}% of other leads!\n\nFind your calendar invite below:\n\n[Click Here]({url1 if int_score > thresh else url2})", icon="✅")
+        st.success(f"Thank you for submitting the survey!\n\nFind your calendar invite below:\n\n[Click Here]({url1 if int_score > thresh else url2})", icon="✅")
         nav_to(url1 if int_score > thresh else url2)
     else:
         error_message = generate_error_message(vals, check)
         st.error(error_message, icon="🚨")
-
 
 def generate_error_message(vals, check):
     error = "**Please be sure to complete the following fields:**"
@@ -151,8 +135,9 @@ def generate_error_message(vals, check):
             error = error + f"\n\n{name}"
     if not check:
         error = error + "\n\nI understand my Enrollment Advisor will call me*"
+    if (vals["Phone Number*"] < 7) or (vals["Phone Number*"] > 15):
+        error = error + "\n\nBad number of digits in phone"
     return error
-
 
 def display_footer():
     maglr = '''
@@ -182,14 +167,11 @@ def display_footer():
     '''
     st.markdown(maglr, unsafe_allow_html=True)
 
-
-# Main Streamlit App
 def main():
     set_streamlit_config()
     display_header()
     display_main_content()
     display_footer()
-
 
 if __name__ == "__main__":
     main()
