c8659070feac36a0aab94bd67f64a09c3da7f59c
Caleb Dame <54949405+calebdame@users.noreply.github.com>
2023-10-06 00:23:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+from hubspot import HubSpot
+from hubspot.crm.contacts import SimplePublicObjectInputForCreate
+from hubspot.crm.contacts.exceptions import ApiException
 import streamlit as st
 import datetime
 from featurizer import *
 import re
 import os
+import time
 
 def nav_to(url):
     nav_script = """
@@ -61,24 +65,25 @@ def display_main_content():
     st.markdown("<div align=\"center\">This call will be your gateway to find out more about this program and answer any questions you have! We're excited for you to take this step and look forward to speaking with you.</div><br>", unsafe_allow_html=True)
     st.markdown("##### Provide your information below:")
     with st.form("Answers"):
-        name, country_code, phone_number = get_user_details()
+        fname, lname, country_code, phone_number = get_user_details()
         email, occupation = get_secondary_details()
         long_question_response, has_money, read_brochure = get_additional_details()
         check = st.checkbox("I understand my Enrollment Advisor will call me on the number I provided and at the appointment time I will schedule. I will be ready for the call.*")
         
-        process_form(name, country_code, phone_number, email, occupation, long_question_response, has_money, read_brochure, check)
+        process_form(fname, lname, country_code, phone_number, email, occupation, long_question_response, has_money, read_brochure, check)
 
 def get_user_details():
-    cols = st.columns([4, 2, 4])
+    cols = st.columns([5,5,3,7])
     with cols[0]:
-        name = st.text_input("Full Name*")
+        fname = st.text_input("First Name*")
     with cols[1]:
+        lname = st.text_input("Last Name*")
+    with cols[2]:
         country_list = [f"{i[0]}: {i[1]}" for i in CODES.items()]
         country = st.selectbox("Country Code*", country_list, index=226)
-        country_code = country.split("+")[-1]
-    with cols[2]:
+    with cols[3]:
         phone_number = st.text_input("Phone Number*")
-    return name, country_code, phone_number
+    return fname, lname, country, phone_number
 
 def get_secondary_details():
     cols2 = st.columns([4, 6])
@@ -96,11 +101,12 @@ def get_additional_details():
     read_brochure = st.selectbox("Please confirm you have read our Program Brochure before booking the Enrollment Interview*", b_qs, index=2)
     return long_question_response, has_money, read_brochure
 
-def process_form(name, country_code, phone_number, email, occupation, long_question_response, has_money, read_brochure, check):
+def process_form(fname, lname, country_code, phone_number, email, occupation, long_question_response, has_money, read_brochure, check):
     if st.form_submit_button("Submit"):
         phone = re.sub("[^0-9]", "", phone)
         vals = {
-            "Full Name*": len(name),
+            "First Name*": len(fname),
+            "Last Name*": len(lname),
             "Phone Number*": len(phone_number),
             "Email Address*": len(email),
             "What is your Occupation?*": len(occupation),
@@ -108,14 +114,16 @@ def process_form(name, country_code, phone_number, email, occupation, long_quest
             "What best describes your financial situation?*": len(has_money),
             "Please confirm you have read our Program Brochure before booking the Enrollment Interview*": len(read_brochure)
         }
-        validate_form(vals, check, name, country_code, phone_number, email, occupation, long_question_response, has_money, read_brochure)
+        validate_form(vals, check, fname, lname, country_code, phone_number, email, occupation, long_question_response, has_money, read_brochure)
 
-def validate_form(vals, check, name, country_code, phone_number, email, occupation, long_question_response, has_money, read_brochure):
+def validate_form(vals, check, fname, lname, country_code, phone_number, email, occupation, long_question_response, has_money, read_brochure):
     if all(i != 0 for i in vals.values()) and check and (vals["Phone Number*"] >= 7) and (vals["Phone Number*"] <= 15):
+        ts = datetime.datetime.now()
+        name = fname + " " + lname
         features = [
-            name, country_code + " (" + phone_number, None, email, occupation,
+            name, country_code.split("+")[-1] + " (" + phone_number, None, email, occupation,
             long_question_response, int("cash" in has_money),
-            int("Yes" in read_brochure), datetime.datetime.now()
+            int("Yes" in read_brochure), ts
         ]
         f = Featurizer(*features)
         feats = f.generate_feature_dict()
@@ -123,11 +131,45 @@ def validate_form(vals, check, name, country_code, phone_number, email, occupati
         thresh = 49
         url1 = "https://pages.jayshettycoaching.com/test-jscs-qualified-booking/"
         url2 = "https://pages.jayshettycoaching.com/test-jscs-unqualified-lead/"
+
+        send_to_hubspot(
+            lname, fname, country_code, phone_number, 
+            email, occupation, long_question_response, 
+            has_money, read_brochure, ts, check, 
+            int_score, score
+        )
+        time.sleep(0.25)
         st.success(f"Thank you for submitting the survey!\n\nFind your calendar invite below:\n\n[Click Here]({url1 if int_score > thresh else url2})", icon="✅")
         nav_to(url1 if int_score > thresh else url2)
     else:
         error_message = generate_error_message(vals, check)
         st.error(error_message, icon="🚨")
+
+def send_to_hubspot(lname, fname, country, phone_number, email, occupation, long_question_response, has_money, read_brochure, ts, check, perc_score, score):
+
+    api_client = HubSpot(access_token=os.environ["hs_access_token"])
+    try:
+        simple_public_object_input_for_create = SimplePublicObjectInputForCreate(
+            properties={"email": email,   #string
+                        "firstname": fname,           #string
+                        "lastname": lname,          #string
+                        "phone": f"+{country_code.split("+")[-1]}-{phone_number}",
+                        "jobtitle": occupation,
+                        "what_s_your_goal_": long_question_response,
+                        "what_best_describes_your_financial_situation_":has_money,
+                        "please_confirm_you_have_read_our_program_brochure_before_booking_the_enrollment_interview": read_brochure,
+                        "i_will_be_ready_for_the_call": str(check).lower(),
+                        "lead_score": score,
+                        "lead_percentile_score": perc_score,
+                        "vsl_source": "vsl_3",
+                         "country": country.split(":")[0]
+                         }
+        )
+        api_response = api_client.crm.contacts.basic_api.create(
+            simple_public_object_input_for_create=simple_public_object_input_for_create
+        )
+    except ApiException as e:
+        st.markdown("Exception when creating contact: %s\n" % e)
 
 def generate_error_message(vals, check):
     error = "**Please be sure to complete the following fields:**"
