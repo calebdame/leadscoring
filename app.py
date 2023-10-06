efd7b96f10d86357e94ac2b82961fad548524f7b
Caleb Dame <54949405+calebdame@users.noreply.github.com>
2023-10-06 00:45:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -209,6 +209,7 @@ def display_footer():
     </body><br><br><br>
     '''
     st.markdown(maglr, unsafe_allow_html=True)
+    st.success(st.experimental_get_query_params())
 
 def main():
     set_streamlit_config()
