d7ffb375b6414033a30fe460c17d646fd39f298d
Caleb Dame <54949405+calebdame@users.noreply.github.com>
2023-10-20 14:23:40 -0600

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,19 @@
+THRESH = 49
+UTMS = ['booking-rnc-be',
+ 'booking-c2c-be',
+ 'booking-ovsl-be',
+ 'booking-rnc-a',
+ 'booking-c2c-a',
+ 'booking-ovsl-a',
+ 'booking-rnc-f',
+ 'booking-c2c-f',
+ 'booking-ovsl-f',
+ 'booking-rnc-s',
+ 'booking-c2c-s',
+ 'booking-ovsl-s',
+ 'booking-amb',
+ 'jscs-instant-book',
+ 'booking-be',
+ 'booking-pathway',
+ 'booking-funnel']
+AMOUNT = "7400"
